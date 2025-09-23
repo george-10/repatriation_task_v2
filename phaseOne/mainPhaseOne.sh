21caaadef13bbda477e7b1495fd257c54d1f5e22
georges <george.fayssal@gmail.com>
2025-09-23 12:20:02 +0300

    fix

--- phaseOne/mainPhaseOne.sh
+++ phaseOne/mainPhaseOne.sh
@@ -44,9 +44,9 @@ dbUserName=$(jq -r '.resources[] | select(.properties.administratorLogin != null
 
 echo $dbUserName > $WDIR/dbUserName.txt
 
-#./extractSqlDump.sh $dbUserName $dbDatabase $dbPassword $dbServerHostName
+./extractSqlDump.sh $dbUserName $dbDatabase $dbPassword $dbServerHostName
 
 $HDIR/getRegion.sh $resourceGroup > $WDIR/oldRegion.txt
 $HDIR/getAppServiceSubnet.sh "$resourceGroup" "$appServiceName" > $WDIR/oldSubnetName.txt
-$HDIR/removeFirewallRule.sh "$resourceGroup" "$mysqlServerName"
+#$HDIR/removeFirewallRule.sh "$resourceGroup" "$mysqlServerName"
 echo "Phase One Completed"
