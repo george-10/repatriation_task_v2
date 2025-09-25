a991465bb365e3b204cba16aa20ea8884c33e8ed
georges <george.fayssal@gmail.com>
2025-09-25 14:46:26 +0300

    Fix modify app service template

--- phaseTwo/modifyAppService.sh
+++ phaseTwo/modifyAppService.sh
@@ -31,12 +31,12 @@ jq '(.resources[] | select(.type == "Microsoft.Web/sites") | .properties) |= del
 jq 'del(.resources[] | select(.type == "Microsoft.Web/sites/hostNameBindings"))' \
    $WDIR/resources_template/$oldName.json > tmp.json && mv tmp.json $WDIR/resources_template/$oldName.json
 
+$HDIR/swapValues.sh "$oldName" "$newName" $WDIR/resources_template/$oldName.json
+
 appServicePlanId="$($HDIR/getAppServicePlanId.sh $resourceGroup)"
 jq --arg id "$appServicePlanId" \
   '.parameters |= with_entries(if (.key | test("^serverfarms_.*_externalid$")) then .value.defaultValue = $id else . end)' \
   $WDIR/resources_template/$oldName.json > tmp.json && mv tmp.json $WDIR/resources_template/$oldName.json
-
-
 
 vnetId="$($HDIR/getVnetId.sh $resourceGroup)"
 jq --arg id "$vnetId" \
@@ -51,7 +51,6 @@ jq --arg id "$vnetId" \
 
 $HDIR/swapValues.sh "$oldRegion" "$newRegion" $WDIR/resources_template/$oldName.json
 
-$HDIR/swapValues.sh "$oldName" "$newName" $WDIR/resources_template/$oldName.json
 
 
 $HDIR/swapValues.sh "$oldSubnet" "$newSubnet" $WDIR/resources_template/$oldName.json
