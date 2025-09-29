b2c8960e5a566165262b440c64cbf2be74137cf2
georges <george.fayssal@gmail.com>
2025-09-29 13:15:57 +0300

    Fix sed

--- phaseThree/modifyWordpressDirectory.sh
+++ phaseThree/modifyWordpressDirectory.sh
@@ -1,52 +1,62 @@
 #!/bin/bash
-
-set -e
+set -euo pipefail
 
 WDIR="$HOME/repatriationTask/_work"
 DIR="$HOME/repatriationTask/phaseTwo"
 CONFIG_FILE="$WDIR/wwwroot/wp-config.php"
 
-source $WDIR/urls/new_url.env
-newUrl=$(printf '%s' "$NEW_URL" | sed 's/[\/&\\]/\\&/g')
+source "$WDIR/urls/new_url.env"
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <resource-group>"
   exit 1
 fi
 
-escape_sed_repl() { printf '%s' "$1" | sed 's/[\\&]/\\&/g'; }
+escape_sed_repl() {
+  printf '%s' "$1" | sed 's/[\\&/]/\\&/g'
+}
+
+sed_inplace() {
+  local script=$1
+  local file=$2
+  local tmp
+  tmp="$(mktemp "${file##*/}.XXXXXX")"
+  sed -e "$script" -- "$file" >"$tmp"
+  mv -- "$tmp" "$file"
+}
 
 update_wp_config() {
   local key=$1
-  local value_escaped
-  value_escaped=$(escape_sed_repl "$2")
+  local value=$2
   local file=$3
-  local d=$'\x1f'  # rare delimiter
-  sed -i "s${d}\(define( '${key}', *'\)[^']*\('\s*);\)${d}\1${value_escaped}\2${d}" "$file"
+
+  local repl
+  repl=$(escape_sed_repl "$value")
+
+
+  local d=$'\x1f'
+  local script="s${d}\\(define([[:space:]]*'${key}'[[:space:]]*,[[:space:]]*'\\)[^']*\\('[[:space:]]*);\\)${d}\\1${repl}\\2${d}"
+
+  sed_inplace "$script" "$file"
 }
 
-source $WDIR/urls/new_url.env
-
 ResourceGrp=$1
-NewDomain=$(echo -e '%s\n' "$NEW_URL" | sed 's/&/\\&/g')
 
+# Get new DB host from Azure
+DB_HOST_NEW="$(az mysql flexible-server list \
+  --resource-group "$ResourceGrp" \
+  --query "[0].fullyQualifiedDomainName" -o tsv)"
 
-DB_HOST_NEW=$(az mysql flexible-server list \
-  --resource-group $ResourceGrp \
-  --query "[0].fullyQualifiedDomainName" -o tsv)
 echo "Modifying wp-config.php with new DB_HOST: $DB_HOST_NEW ..."
 
-
 update_wp_config "DB_HOST" "$DB_HOST_NEW" "$CONFIG_FILE"
-
-update_wp_config "DOMAIN_CURRENT_SITE" "$NewDomain" "$CONFIG_FILE"
-
+update_wp_config "DOMAIN_CURRENT_SITE" "$NEW_URL" "$CONFIG_FILE"
 
 echo -e "wp-config.php updated successfully! \n"
 echo "creating zip file"
 
-cd $WDIR/wwwroot
+cd "$WDIR/wwwroot"
 zip -qr "wordpress.zip" *
 mv wordpress.zip ./..
-cd $DIR
+cd "$DIR"
 echo -e "zip file created successfully \n"
