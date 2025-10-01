43a137d137778893d149750e5467ef1e270cf40e
Ubuntu <localadmin@vm-agent-fc-001.nussyhjkdfeexo2nmmkwkrst0d.parx.internal.cloudapp.net>
2025-10-01 10:12:00 +0000

    Modify sql dump urls

--- phaseThree/modifySqlDump.sh
+++ phaseThree/modifySqlDump.sh
@@ -16,3 +16,16 @@ echo "Replacing $oldUrl with $newUrl in sqldump ..."
 sed -i "s|$oldUrl|$newUrl|g" "$WDIR/dump.sql"
 
 echo -e "URL changed successfully\n"
+echo "Changing the string number of the following..."
+
+grep -Ro 's:[0-9]\+:\\"https\?://[^"]\+\\";' "$WDIR/dump.sql"
+
+echo -e "\nChanging...\n"
+
+perl -pi -e 's|s:(\d+):\\\"(https?:\/\/[^\\"]+)\\\";| "s:" . length($2) . ":\\\"$2\\\";" |ge' "$WDIR/dump.sql"
+
+echo "Change done. New urls: "
+
+grep -Ro 's:[0-9]\+:\\"https\?://[^"]\+\\";' "$WDIR/dump.sql"
+
+echo -e "Sql dump modified successfully\n"
